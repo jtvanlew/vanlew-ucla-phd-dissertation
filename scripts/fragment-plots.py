7be79a2bab64d46b0041920369773fd40832945b
jtvanlew <jonvanlew@gmail.com>
2015-03-16 17:37:36 -0700

    adds script files for fragment study
    =

--- scripts/fragment-plots.py
+++ scripts/fragment-plots.py
@@ -1,24 +1,35 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-rstar_1 = np.linspace(0,1,100)
+rstar_1 = np.linspace(0.2,1,100)
 
 data = np.loadtxt('pebble_packing_radii.txt')
+rstar_2 = {}
 for row in data:
-	n = data[:,0]
-	rstar_2 = data[:,1]
+    n = data[:,0]
+    rstar = data[:,1]
+for i, n in enumerate(n):
+    rstar_2[str(int(n))] = rstar[i]
 
+N = np.round(1/rstar_1**3,0)
+Vstar = []
+Pf = []
+for j, n in enumerate(N):
+    n = str(int(n))
+    if n in rstar_2:
+        #print n + ' particle fragment(s)'
+        #print 'r*1 = '+str(rstar_1[j])
+        #print 'r*2 = '+str(rstar_2[n])
+        Pf.append(n)
+        Vstar.append((rstar_1[j]/rstar_2[n])**3)
+    else:
+        print 'radius ratio for '+ n + ' pebbles is not in the dictionary'
 
-
-plt.close('all')
+plt.plot(Pf, Vstar)
 plt.figure(1)
-plt.xlabel(r'Dimensionless time ($t/\tau$)')
-plt.ylabel(r'Dimensionless total heat removed, $Q^*$')
-plt.title(r'Biot Number = %s'%(np.round(Bi,2)))
-plt.plot(Fo, Q1, label = 'Lumped capacitance model (LC)')
-plt.plot(Fo, Q2,  marker = 'o', label = 'LC with Jeffreson correction (JC)')
-plt.plot(Fo, Q3, label = 'Exact solution with thermal gradient (TG)')
-plt.ylim([0, 1])
+plt.xlabel(r'Number of particles')
+plt.ylabel(r'Dimensionless Volume')
+plt.xlim([0, 72])
 plt.legend(loc='best')
 
 plt.show()
