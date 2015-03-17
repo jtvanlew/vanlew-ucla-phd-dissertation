aa71889b2233707dcc310ae7807dfc6025a2fceb
jtvanlew <jonvanlew@gmail.com>
2015-03-16 19:53:26 -0700

    adds figures and almost finishes the section on pebble crushing fragments

--- scripts/fragment-plots.py
+++ scripts/fragment-plots.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-rstar_1 = np.linspace(0.2,1,100)
+rstar_1 = np.linspace(0.2,1,10000)
 
 data = np.loadtxt('pebble_packing_radii.txt')
 rstar_2 = {}
@@ -20,16 +20,21 @@ for j, n in enumerate(N):
         #print n + ' particle fragment(s)'
         #print 'r*1 = '+str(rstar_1[j])
         #print 'r*2 = '+str(rstar_2[n])
-        Pf.append(n)
+        Pf.append(rstar_1[j])
         Vstar.append((rstar_1[j]/rstar_2[n])**3)
     else:
         print 'radius ratio for '+ n + ' pebbles is not in the dictionary'
-
+plt.figure(1)
 plt.plot(Pf, Vstar)
 plt.figure(1)
-plt.xlabel(r'Number of particles')
+plt.xlabel(r'Radius Ratio ($R_c/R_p$)')
 plt.ylabel(r'Dimensionless Volume')
-plt.xlim([0, 72])
+plt.xlim([0, 1])
 plt.legend(loc='best')
+
+plt.figure(2)
+plt.plot(rstar_1, N)
+plt.xlabel(r'Radius Ratio ($R_c/R_p$)')
+plt.ylabel('Pebble Fragments (rounded to whole number)')
 
 plt.show()
