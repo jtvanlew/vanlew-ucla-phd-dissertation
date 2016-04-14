9b213d51c6cae7dff0456cf59517091750bc8008
Jon Van Lew <jonvanlew@gmail.comjonvanlew@gmail.com>
2016-04-14 10:10:52 -0700

    edits at campus

--- scripts/conduction-vs-radiation-plot.py
+++ scripts/conduction-vs-radiation-plot.py
@@ -18,13 +18,14 @@ Estar = E/(2*(1-nu**2))
 
 Hc = 2*ks*(3/4 * Rstar/Estar)**(1/3) * (10**(1/3))
 
-ep = 0.8
+ep = 0.5
 sigma = 5.67e-8
-A = 2*np.pi*R**2
+A = 4*np.pi*R**2
+Fij = 0.075
+Hr = ep*sigma*A*Fij
 
-Hr = ep*sigma*A
-
-T1 = np.linspace(300, 800, 10)
+T1 = np.linspace(300, 800, 100)
+T1 = T1 + 273
 
 for i, T in enumerate(T1):
     T2 = np.linspace(T - 100, T, 50)
@@ -35,15 +36,19 @@ for i, T in enumerate(T1):
     Qr[0:-1] = Qr[0:-1]/Qc[0:-1]
     Qc[0:-1] = Qc[0:-1]/Qc[0:-1]
     
+    for q in Qr:
+        if q > 0.15:
+            print(T-273)
+            break
+    # plt.figure(i)
+    # plt.plot(T2[0:-1]-273, Qc[0:-1], linewidth = 2, color = color_idx[0], label='Conduction')
+    # plt.plot(T2[0:-1]-273, Qr[0:-1], linewidth = 2, color = color_idx[1], label="Radiation")
+    # plt.xlim(min(T2)-273, max(T2)-273)
+    # plt.ylim(0, 1.1)
+    # plt.xlabel(r"$T_2$ (C)")
+    # plt.ylabel("Normalized heat transfer between pebbles")
+    # plt.legend(loc='best')
+    # plt.grid('on')
+    # plt.savefig('../figures/conduction-vs-radiation/%s.png'%(max(np.round(T2-273,0))), format='PNG')
 
-    plt.figure(i)
-    plt.plot(T2[0:-1], Qc[0:-1], linewidth = 2, color = color_idx[0], label='Conduction')
-    plt.plot(T2[0:-1], Qr[0:-1], linewidth = 2, color = color_idx[1], label="Radiation")
-    plt.xlim(min(T2), max(T2))
-    plt.ylim(0, 1.1)
-    plt.xlabel(r"$T_2$ (C)")
-    plt.ylabel("Normalized heat transfer between pebbles")
-    plt.legend(loc='best')
-    plt.grid('on')
-    plt.savefig('../figures/conduction-vs-radiation/%s.png'%(max(np.round(T2,0))), format='PNG')
-
+# plt.show()
